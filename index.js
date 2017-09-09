11b89d6063fe75dfe7102d41f9091c942226545c
Anthony Fields <janthonyfields@gmail.com>
2017-09-09 23:27:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function updateObjectWithKeyAndValue( obj, key, val ) {
 function destructivelyUpdateObjectWithKeyAndValue( obj, key, val ) {
   return Object.assign( obj, { [key]: val } );
 }
+
+deleteFromObjectByKey( obj, key ) {
+  var objClone = Object.assign( {}, obj );
+  delete objClone[key];
+  return objClone;
+}
